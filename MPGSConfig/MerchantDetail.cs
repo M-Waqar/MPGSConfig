0d164817e0b7328b2c1af3e340b7c453a8e52eda
muhammad Waqar <muhammadwaqar552@gmail.com>
2024-08-02 17:44:44 +0400

    Add project files.

---
+++ MPGSConfig/MerchantDetail.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPGSConfig
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class _3DS1
+    {
+        public string merchantId { get; set; }
+        public string cardAcceptorId { get; set; }
+        public string cardAcceptorTerminalId { get; set; }
+    }
+
+    public class AcquirerLink
+    {
+        public ADCB031200S2I01 ADCB_031200_S2I01 { get; set; }
+        public ADCBS2I01 ADCB_S2I01 { get; set; }
+    }
+
+    public class ADCB031200S2I01
+    {
+        public bool accountUpdaterEnabled { get; set; }
+        public string acquirerId { get; set; }
+        public List<string> allowableTransactionFrequency { get; set; }
+        public List<string> allowableTransactionSource { get; set; }
+        public Authentication authentication { get; set; }
+        public string bankMerchantId { get; set; }
+        public List<string> cardType { get; set; }
+        public List<string> currency { get; set; }
+        public string defaultTransactionFrequency { get; set; }
+        public string defaultTransactionSource { get; set; }
+        public List<string> enforceCscOnTransactionSource { get; set; }
+        public string paymentType { get; set; }
+        public string status { get; set; }
+        public List<string> terminalId { get; set; }
+    }
+
+    public class ADCBS2I01
+    {
+        public bool accountUpdaterEnabled { get; set; }
+        public string acquirerId { get; set; }
+        public List<string> allowableTransactionFrequency { get; set; }
+        public List<string> allowableTransactionSource { get; set; }
+        public Authentication authentication { get; set; }
+        public string bankMerchantId { get; set; }
+        public List<string> cardType { get; set; }
+        public List<string> currency { get; set; }
+        public string defaultTransactionFrequency { get; set; }
+        public string defaultTransactionSource { get; set; }
+        public List<string> enforceCscOnTransactionSource { get; set; }
+        public string paymentType { get; set; }
+        public string status { get; set; }
+        public List<string> terminalId { get; set; }
+    }
+
+    public class Address
+    {
+        public string city { get; set; }
+        public string countryCode { get; set; }
+        public string postcode { get; set; }
+        public string stateProvince { get; set; }
+        public string street1 { get; set; }
+        public string street2 { get; set; }
+    }
+
+    public class Authentication
+    {
+        public MasterCardSecureCode masterCardSecureCode { get; set; }
+        public VerifiedByVisa verifiedByVisa { get; set; }
+    }
+
+    public class Download
+    {
+        public int paymentAuthenticationMaximumRows { get; set; }
+    }
+
+    public class Gatekeeper
+    {
+        public string sendGatewayMerchantId { get; set; }
+        public string serviceLevel { get; set; }
+    }
+
+    public class IpCountry
+    {
+        public bool rejectAnonymousProxy { get; set; }
+        public bool rejectUnknownCountry { get; set; }
+    }
+
+    public class MasterCardSecureCode
+    {
+        [JsonProperty("3DS1")]
+        public _3DS1 _3DS1 { get; set; }
+    }
+
+    public class _Merchant
+    {
+        public AcquirerLink acquirerLink { get; set; }
+        public Address address { get; set; }
+        public string apiAuthenticationMode { get; set; }
+        public string cardMaskingFormat { get; set; }
+        public string categoryCode { get; set; }
+        public string contactName { get; set; }
+        public string defaultOrderCertainty { get; set; }
+        public Download download { get; set; }
+        public string email { get; set; }
+        public string goodsDescription { get; set; }
+        public string id { get; set; }
+        public string locale { get; set; }
+        public MerchantManagedRiskAssessment merchantManagedRiskAssessment { get; set; }
+        public string name { get; set; }
+        public PartnerManagedRiskAssessment partnerManagedRiskAssessment { get; set; }
+        public List<string> privilege { get; set; }
+        public bool sendWelcomeEmail { get; set; }
+        public List<string> service { get; set; }
+        public string state { get; set; }
+        public string systemCapturedCardMaskingFormat { get; set; }
+        public string timeZone { get; set; }
+        public TransactionFiltering transactionFiltering { get; set; }
+        public DateTime updated { get; set; }
+        public string webSite { get; set; }
+    }
+
+    public class MerchantManagedRiskAssessment
+    {
+        public string assessmentInitiation { get; set; }
+        public string enableRiskAssessment { get; set; }
+        public Gatekeeper gatekeeper { get; set; }
+        public Profile profile { get; set; }
+        public string providerId { get; set; }
+    }
+
+    public class PartnerManagedRiskAssessment
+    {
+        public TransactionRiskAssessment transactionRiskAssessment { get; set; }
+    }
+
+    public class Profile
+    {
+        public string defaultActionWhenUnableToPerformRiskAssessment { get; set; }
+        public string id { get; set; }
+        public string supportRiskScoringFor { get; set; }
+    }
+
+    public class MerchantDetail
+    {
+        [JsonProperty("Merchant")]
+        public _Merchant _merchant { get; set; }
+        public string result { get; set; }
+    }
+
+    public class TransactionFiltering
+    {
+        public bool dynamic3DSecure { get; set; }
+        public IpCountry ipCountry { get; set; }
+    }
+
+    public class TransactionRiskAssessment
+    {
+        public string configuration { get; set; }
+    }
+
+    public class VerifiedByVisa
+    {
+        [JsonProperty("3DS1")]
+        public _3DS1 _3DS1 { get; set; }
+    }
+}
---
+++ MPGSConfig/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace MPGSConfig
+{
+    public class Merchant
+    {
+        public string id { get; set; }
+        public DateTime updated { get; set; }
+    }
+
+    public class Mso
+    {
+        public string id { get; set; }
+    }
+
+    public class MerchnatList
+    {
+        public List<Merchant> merchant { get; set; }
+        public Mso mso { get; set; }
+        public string result { get; set; }
+    }
+    public class Program
+    {
+        public static string username = "";
+        public static string password = ""; 
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Program start");
+                
+                MerchnatList list = getMPGSMerchantList();
+                getMPGSMerchantConfig(list.merchant);
+
+                //List<Merchant> list = new List<Merchant>() { 
+                //    new Merchant(){ id = "760076303278", updated= DateTime.Now },
+                //    new Merchant(){ id = "120815000239", updated= DateTime.Now },
+                //    new Merchant(){ id = "120811000013", updated= DateTime.Now },
+                //    new Merchant(){ id = "120816200023", updated= DateTime.Now },
+                //    new Merchant(){ id = "120815000287", updated= DateTime.Now },
+                //    new Merchant(){ id = "120816200017", updated= DateTime.Now },
+                //};
+                //getMPGSMerchantConfig(list);
+
+                Console.WriteLine("Program ends");
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
+        public static MerchnatList getMPGSMerchantList()
+        {
+            try
+            {
+                string baseUrl = "https://adcb.gateway.mastercard.com/api/rest/version/82/mso/ADCB_MSO_01/merchant";
+                var options = new RestClientOptions(baseUrl);
+                options.Authenticator = new HttpBasicAuthenticator(username, password);
+                var client = new RestClient(options);
+
+                RestRequest request = new RestRequest();
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    MerchnatList MerchnatList = JsonConvert.DeserializeObject<MerchnatList>(response.Content);
+                    using (var file = File.CreateText(@"C:\Users\muham\Desktop\Projects\Config_MPGS\mpgsmid.csv"))
+                    {
+                        file.WriteLine("MPGS_MID");
+                        foreach (Merchant merchant in MerchnatList.merchant)
+                        {
+                            file.WriteLine(merchant.id);
+                        }
+                    }
+                    return MerchnatList;
+                }
+                else
+                {
+                    Console.WriteLine("error while calling MPGS Merchants API");
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void getMPGSMerchantConfig(List<Merchant> list)
+        {
+            try
+            {
+                AppendText("MID,NAME,STATUS,ACQ LINK ID,ACQ LINK STATUS,ACQ LINK ID,ACQ LINK STATUS");
+                foreach (Merchant item in list)
+                {
+                    string baseUrl = "https://adcb.gateway.mastercard.com/api/rest/version/82/mso/ADCB_MSO_01/merchant/" + item.id;
+                    var options = new RestClientOptions(baseUrl);
+                    options.Authenticator = new HttpBasicAuthenticator(username, password);
+                    var client = new RestClient(options);
+
+                    RestRequest request = new RestRequest();
+                    var response = client.Execute(request);
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        MerchantDetail merchantDetail = JsonConvert.DeserializeObject<MerchantDetail>(response.Content);
+                        string MerchantData = string.Format("{0},{1},{2},{3},{4},{5},{6}",
+                            merchantDetail._merchant.id,
+                            merchantDetail._merchant.name,
+                            merchantDetail._merchant.state,
+                            merchantDetail._merchant.acquirerLink.ADCB_031200_S2I01 != null ? merchantDetail._merchant.acquirerLink.ADCB_031200_S2I01.acquirerId : "",
+                            merchantDetail._merchant.acquirerLink.ADCB_031200_S2I01 != null ? merchantDetail._merchant.acquirerLink.ADCB_031200_S2I01.status : "",
+                            merchantDetail._merchant.acquirerLink.ADCB_S2I01 != null ? merchantDetail._merchant.acquirerLink.ADCB_S2I01.acquirerId : "",
+                            merchantDetail._merchant.acquirerLink.ADCB_S2I01 != null ? merchantDetail._merchant.acquirerLink.ADCB_S2I01.status : "");
+
+                        AppendText(MerchantData);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error while calling MPGS Merchant Detail API of MID: " + item.id);
+                    }
+                    Console.WriteLine("Writing config of MID: " + item.id);
+                    Thread.Sleep(4000);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void AppendText(string text)
+        {
+            string path = @"C:\Users\muham\Desktop\Projects\Config_MPGS\mpgsmidconfig.csv";
+            File.AppendAllText(path, text + Environment.NewLine);
+        }
+    }
+}
